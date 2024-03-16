d5b9d9905ad5d9ce63fa4f9d758a4dcefc0dde6b
Oleg Mihailik <mihailik@gmail.com>
2024-03-16 10:27:35 +0000

    Allow toggling formatting on a current word.

--- src/format-actions/apply-modifier-to-selection.js
+++ src/format-actions/apply-modifier-to-selection.js
@@ -8,14 +8,51 @@ import { applyModifier } from '../unicode-styles/apply-modifier';
  * @param {boolean} [remove]
  */
 export function applyModifierToSelection(cmView, modifier, remove) {
-  const { from, to } = cmView.state.selection.main;
-  const selection = cmView.state.sliceDoc(from, to);
+  let { from, to } = cmView.state.selection.main;
+  let selection = cmView.state.sliceDoc(from, to);
+  let expandedToWord = false;
+
+  let hintLead = 0;
+  if (!selection) {
+    // expand selection
+    let leadText = cmView.state.sliceDoc(0, from);
+    let trailText = cmView.state.sliceDoc(to);
+
+    let lastWordMatch = /[^\s\r\n]*$/i.exec(leadText);
+    let lastWordIndex = lastWordMatch ? lastWordMatch.index : 0;
+    let leadForWord = leadText.slice(lastWordIndex);
+    if (leadForWord.length && leadForWord.length < 100) {
+      selection = leadForWord;
+      from -= leadForWord.length;
+      hintLead = leadForWord.length;
+      leadText = leadText.slice(0, lastWordIndex);
+      expandedToWord = true;
+    }
+
+    var firstWordMatch = /^[^\s\r\n]*/i.exec(trailText);
+    var trailForWord = firstWordMatch ? firstWordMatch[0] : '';
+    if (trailForWord.length && trailForWord.length < 100) {
+      selection += trailForWord;
+      to += trailForWord.length;
+      trailText = trailText.slice(trailForWord.length);
+      expandedToWord = true;
+    }
+  }
+
+  if (!selection) return false;
+
   const modifiedSelection = applyModifier(selection, modifier, remove);
   if (modifiedSelection === selection) return;
 
+  const maintainCursor = !hintLead ? 0 :
+    from + applyModifier(selection.slice(0, hintLead), modifier, remove).length;
   cmView.dispatch(
     {
       changes: { from, to, insert: modifiedSelection },
+      selection: !maintainCursor ? undefined : {
+        anchor: maintainCursor,
+        head: maintainCursor
+      }
     }
   );
 
