b1283b0a097bde792e9944c21df6cf52c8432e36
Oleg Mihailik <mihailik@gmail.com>
2024-08-23 23:31:39 +0100

    Mode switcher prototype.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 import { initCodeMirror } from './editor';
 import { version } from '../package.json';
+import { showModeSwitcher } from './mode-switcher';
 
 /** @type {import('codemirror').EditorView & { residualModifiers?: string } } */
 export var cmView;
@@ -10,4 +11,11 @@ if (typeof window !== 'undefined' && typeof window?.alert === 'function') {
   const versionDIV = document.getElementById('version');
   if (versionDIV) versionDIV.textContent = 'v' + version;
   initCodeMirror();
+
+  const moreModes = document.getElementById('moreModes');
+  if (moreModes) {
+    moreModes.addEventListener('click', () => {
+      showModeSwitcher();
+    });
+  }
 }
---
+++ src/mode-switcher/index.js
@@ -0,0 +1,3 @@
+// @ts-check
+
+export { showModeSwitcher } from "./show-mode-switcher";
