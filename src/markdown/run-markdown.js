217d45a825f622cc6ccc4c7536f141a46994defb
Oleg Mihailik <mihailik@gmail.com>
2024-09-05 22:51:12 +0100

    Moving execution into the bottom, displaying results too.

--- src/markdown/run-markdown.js
+++ src/markdown/run-markdown.js
@@ -11,6 +11,9 @@ import { commonmark } from '@milkdown/preset-commonmark';
 import { gfm } from '@milkdown/preset-gfm';
 import { Plugin, PluginKey, Selection, TextSelection, Transaction } from '@milkdown/prose/state';
 
+import { EditorState as CodeMirrorEditorState, StateEffect as CodeMirrorStateEffect } from '@codemirror/state';
+import { EditorView as CodeMirrorEditorView, showPanel as codeMirrorShowPanel } from '@codemirror/view';
+
 // import { nord } from '@milkdown/theme-nord';
 
 import "@milkdown/crepe/theme/common/style.css";
@@ -28,6 +31,8 @@ import './katex-part.css';
 import './milkdown-neat.css';
 
 const defaultText = '🆃𝘆𝗽𝗲  ৳໐  🆈𝒐𝓾𝓻𝓼𝒆𝓵𝓯';
+
+const focusEffect = CodeMirrorStateEffect.define(undefined);
 
 /**
  * @param {HTMLElement} host
@@ -53,6 +58,98 @@ export async function runMarkdown(host, markdownText) {
     },
     featureConfigs: {
       "code-mirror": {
+        extensions: [
+          CodeMirrorEditorView.focusChangeEffect.of((state, focusing) => {
+            console.log('focus change effect ', state, focusing);
+            return focusEffect.of(null);
+          }),
+          codeMirrorShowPanel.of(
+            /** @param {CodeMirrorEditorView} view */
+            (view) => {
+              let dom = document.createElement('div');
+              dom.className = 'run-script-bar';
+              const runButton = document.createElement('button');
+              runButton.textContent = 'run ⏵';
+              runButton.className = 'run-script-button';
+              runButton.addEventListener('click', (e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                const scriptText = view.state.doc.toString();
+                execScript(scriptText);
+              });
+              dom.appendChild(runButton);
+
+              const smallStatusLabel = document.createElement('span');
+              smallStatusLabel.className = 'run-script-status';
+              dom.appendChild(smallStatusLabel);
+
+              const largeResultArea = document.createElement('div');
+              largeResultArea.className = 'run-script-result';
+              dom.appendChild(largeResultArea);
+
+              var scriptExecution;
+
+              return {
+                dom,
+                update(update) {
+                  if (update.docChanged) {
+                    resetScriptResult();
+                  }
+                }
+              };
+
+              /**
+               * @param {string} scriptText
+               */
+              async function execScript(scriptText) {
+                if (!scriptText) return;
+
+                scriptExecution = {};
+                runButton.disabled = true;
+                runButton.textContent = 'running ...';
+                smallStatusLabel.textContent = '';
+                largeResultArea.textContent = '';
+                const startRun = Date.now();
+
+                try {
+                  const result = await execScriptIsolated(scriptText);
+
+                  try {
+                    if (result === null)
+                      largeResultArea.textContent = 'null';
+                    else if (result === undefined)
+                      largeResultArea.textContent = 'void';
+                    else if (typeof result === 'object' || typeof result === 'string')
+                      largeResultArea.textContent = JSON.stringify(result, null, 2);
+                    else
+                      largeResultArea.textContent = typeof result + ': ' + String(result);
+                  } catch (jsonError) {
+                    try {
+
+                    } catch (error) {
+                      largeResultArea.textContent = 'Result could not be displayed: ' + (error?.stack || error);
+                    }
+                  }
+
+                  largeResultArea.className = 'run-script-result run-script-result-success';
+                  runButton.disabled = false;
+                  runButton.textContent = 'run ⏵';
+                  smallStatusLabel.textContent = `completed in ${Date.now() - startRun} ms`;
+                } catch (err) {
+                  largeResultArea.textContent = err?.stack || err;
+
+                  largeResultArea.className = 'run-script-result run-script-result-error';
+                  runButton.disabled = false;
+                  runButton.textContent = 'run ⏵';
+                  smallStatusLabel.textContent = `failed in ${Date.now() - startRun} ms`;
+                }
+
+              }
+
+              function resetScriptResult() {
+              }
+            })
+        ]
       },
       toolbar: {
       }
@@ -96,12 +193,6 @@ export async function runMarkdown(host, markdownText) {
             },
           }
         });
-
-        setTimeout(() => {
-          const editorView = ctx.get(editorViewCtx);
-
-          wireRunCodeblock(editorView);
-        }, 200);
 
         return [...plugins, pluginCarryUnicodeConversions];
       });
@@ -164,58 +255,29 @@ export async function runMarkdown(host, markdownText) {
   /** @type {HTMLIFrameElement & { runThis(code: string); }} */
   var ifr;
 
-  /**
-   * @param {import("prosemirror-view").EditorView} editorView
-   */
-  function wireRunCodeblock(editorView) {
-    console.log('wire run code-block ', editorView, editorView.dom);
-    editorView.dom.addEventListener('mousedown', async (e) => {
+  /** @param {string} scriptText */
+  async function execScriptIsolated(scriptText) {
+    if (!ifr) {
+      ifr = /** @type {typeof ifr} */(document.createElement('iframe'));
+      ifr.style.cssText =
+        'position: absolute; left: -200px; top: -200px; width: 20px; height: 20px; pointer-events: none; opacity: 0.01;'
 
-      const tools = /** @type {HTMLElement} */(e.target);
-      if (tools?.className !== 'tools') {
-        return;
-      }
+      ifr.src = 'about:blank';
 
-      const toolsBorders = tools.getBoundingClientRect();
-      if (e.x < toolsBorders.right - toolsBorders.height * 2.3) return;
+      document.body.appendChild(ifr);
 
-      const milkdownCodeBlock = /** @type {HTMLElement} */(tools.closest('milkdown-code-block'));
-      if (!milkdownCodeBlock) return;
+      await new Promise(resolve => setTimeout(resolve, 10));
 
-      const text = milkdownCodeBlock.pmViewDesc?.node?.textContent;
-      if (!text) return;
+      ifr.contentDocument?.write(
+        '<script>window.runThis = function(code) { return eval(code) }</script>'
+      );
 
-      console.log('mouse down on tools ', tools, '\n\n', text);
+      ifr.runThis = /** @type {*} */(ifr.contentWindow).runThis;
+      delete /** @type {*} */(ifr.contentWindow).runThis;
+    }
 
-      e.preventDefault();
-      e.stopPropagation();
-
-      if (!ifr) {
-        ifr = /** @type {typeof ifr} */(document.createElement('iframe'));
-        ifr.style.cssText =
-          'position: absolute; left: -200px; top: -200px; width: 20px; height: 20px; pointer-events: none; opacity: 0.01;'
-        
-        ifr.src = 'about:blank';
-
-        document.body.appendChild(ifr);
-
-        await new Promise(resolve => setTimeout(resolve, 10));
-
-        ifr.contentDocument?.write(
-          '<script>window.runThis = function(code) { return eval(code) }</script>'
-        );
-
-        ifr.runThis = /** @type {*} */(ifr.contentWindow).runThis;
-        delete /** @type {*} */(ifr.contentWindow).runThis;
-      }
-
-      try {
-        const result = await ifr.runThis(text);
-        alert(result);
-
-      } catch (err) {
-        alert('ERROR ' + err);
-      }
-    });
+    return await ifr.runThis(scriptText)
   }
+
 }
+
