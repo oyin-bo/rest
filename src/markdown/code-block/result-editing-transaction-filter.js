c2ffc97b6628f502c2f71fd8b95fe656153ab93a
Oleg Mihailik <mihailik@gmail.com>
2024-09-16 07:46:42 +0100

    Improving the code around readonly handling of execution result blocks.

--- src/markdown/code-block/result-editing-transaction-filter.js
+++ src/markdown/code-block/result-editing-transaction-filter.js
@@ -11,30 +11,62 @@ export function createResultEditingTransactionResult() {
       // let the code result changes flow normally
       if (tr.getMeta('setLargeResultAreaText')) return true;
 
-      let codeBlockResults = [];
-      state.doc.nodesBetween(0, state.doc.content.size, (node, pos) => {
-        if (node.type.name === 'code_block_result') {
-          codeBlockResults.push({ pos, nodeSize: node.nodeSize });
-        }
-      });
+      const codeBlockResultNodes = getNodesWithPositions(
+        state.doc,
+        node => node.type.name === 'code_block_result');
+
+      const codeBlockNodes = getNodesWithPositions(
+        state.doc,
+        node => node.type.name === 'code_block');
 
       for (const step of tr.steps) {
         const replaceStep = /** @type {import('prosemirror-transform').ReplaceStep} */ (step);
         if (replaceStep.from >= 0 && replaceStep.to >= 0) {
-          for (const resultSpan of codeBlockResults) {
-            const resultSpanAffected = replaceStep.from < resultSpan.pos + resultSpan.nodeSize &&
-              replaceStep.to > resultSpan.pos;
-            if (resultSpanAffected) {
+          for (const { pos, node } of codeBlockResultNodes) {
+            if (spanOverlap(replaceStep, pos, node.nodeSize))
               return false;
-            }
           }
         }
       }
 
-      console.log('passed transaction ', tr, codeBlockResults);
+      console.log('passed transaction ', tr, codeBlockResultNodes);
 
       return true;
     },
   });
+
   return pluginFilterEditing;
+}
+
+/**
+ * @param {import("prosemirror-model").Node} doc
+ * @param {(node: import('prosemirror-model').Node, pos: number) => boolean} filter
+ */
+function getNodesWithPositions(doc, filter) {
+  /**
+   * @type {{
+   *  node: import ("prosemirror-model").Node,
+   *  pos: number
+   * }[]}
+   */
+  let resultNodes = [];
+  doc.nodesBetween(0, doc.content.size, (node, pos) => {
+    if (filter(node, pos)) {
+      resultNodes.push({ node, pos });
+    }
+  });
+  return resultNodes;
+}
+
+/**
+ * @param {import('prosemirror-transform').ReplaceStep} replaceStep
+ * @param {number} offset
+ * @param {number} length
+ */
+function spanOverlap(replaceStep, offset, length) {
+  const resultSpanAffected = replaceStep.from < offset + length &&
+    replaceStep.to > offset;
+  if (resultSpanAffected) {
+    return true;
+  }
 }
