60a038b6e4e9e94b65a04ef3c9cd7b6fc3aea0ae
Oleg Mihailik <mihailik@gmail.com>
2024-10-11 22:56:06 +0100

    While loading ***-ifrwrk subdomain, refer to parent domain assets: styles and scripts. Saves noticeable space and time.

--- src/core.js
+++ src/core.js
@@ -11,5 +11,20 @@ if (location.hostname && location.hostname.toLowerCase() !== 'localhost') {
   document.documentElement.classList.add('loaded-site');
 }
 
+const baseHref = 
+  location.hostname.indexOf('-ifrwrk') >= 0 ?
+    String(location).replace(/[a-z0-9]+-ifrwrk\./, '') :
+    '';
+
+const link = document.createElement('link');
+link.rel = 'stylesheet';
+link.href = baseHref + 'index.css';
+
+const script = document.createElement('script');
+script.src = baseHref + 'index.js';
+
+(document.body || document.head).appendChild(link);
+(document.body || document.head).appendChild(script);
+
 window['tty_load_basic_dom'] = function () {
 };
