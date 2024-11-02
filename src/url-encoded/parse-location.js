9c6994a7a14a34ee476f2d5133b0b8c0e533f70c
Oleg Mihailik <mihailik@gmail.com>
2024-11-02 11:27:35 +0000

    Treat mocku.me/nt same as mocku.me/mt/ with trailing slash.

--- src/url-encoded/parse-location.js
+++ src/url-encoded/parse-location.js
@@ -26,7 +26,7 @@ export function parseLocation(location) {
         source: 'path',
         baseHref: '/',
         pathLead: 'nt/',
-        payload: location.pathname.slice(location.pathname.indexOf('/', 1) + 1).replace(/^nt\//, '')
+        payload: location.pathname.slice(location.pathname.indexOf('/', 1) + 1).replace(/^nt(\/|$)/, '')
       };
     } else if (/\.vscode/i.test(location.host)) {
       var matchIndexHtml = /\/(index|404)\.html\b/i.exec(location.pathname || '');
