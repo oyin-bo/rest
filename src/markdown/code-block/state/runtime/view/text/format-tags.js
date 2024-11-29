95bcba4afc827f08339b428297a984d20be6e4e8
Oleg Mihailik <mihailik@gmail.com>
2024-11-29 07:54:50 -0200

    Switching to use generic tag toggle, to be extended for charts later.

--- src/markdown/code-block/state/runtime/view/text/format-tags.js
+++ src/markdown/code-block/state/runtime/view/text/format-tags.js
@@ -1,71 +1,140 @@
+// @ts-check
+
 /**
  * @typedef {{
  *  path: string,
  *  state: Record<string, any>,
- *  invalidate: () => void,
- *  formats: { [format: string]: FormatTagOption }
+ *  invalidate(): void,
+ *  formats: { [format: string]: () => (value: any) => FormatTagOption },
+ *  json: (value: any) => import('..').RenderedContent[]
  * }} FormatTagParams
  */
 
 /**
  * @typedef {{
- *  button: (tagButton: HTMLElement, selected: boolean) => void,
- *  content: () => import('..').RenderedContent[] | import('..').RenderedContent
- *  onSelected?: () => void
+ *  preference: number,
+ *  button: HTMLElement,
+ *  render: import('..').RenderedContent[] | import('..').RenderedContent
  * }} FormatTagOption
  */
 
 const TAG_WIDGET_SUFFIX = '.tagWidget';
+const TAG_VIEW_SUFFIX = '.tagView';
 
 /**
  * @param {FormatTagParams} params
  */
 export function formatTagWidget(params) {
-  /** @type {import('..').RenderedWidget & { viewType: string } | undefined} */
+
+  /** @type {ReturnType<typeof createToggleWidget> | undefined} */
   let toggleWidget = params.state[TAG_WIDGET_SUFFIX];
   if (!toggleWidget) {
-    toggleWidget = {
-      viewType: 'json',
-      widget: () => {
-        const togglePanel = document.createElement('span');
-        togglePanel.className = 'inline-view-toggle-panel';
-
-        const jsonButton = createTagToggle('json', {
-          button: () => {},
-          content: () => null
-        });
-        jsonButton.textContent = '{}';
-        togglePanel.appendChild(jsonButton);
-
-        for (const [format, option] of Object.entries(params.formats)) {
-          createTagToggle(format, option);
-        }
-
-        return togglePanel;
-      }
-    };
+    toggleWidget = createToggleWidget();
   }
 
   return toggleWidget;
 
-  /**
-   * @param {string} format
-   * @param {FormatTagOption} option
-   */
-  function createTagToggle(format, option) {
-    const tagButton = document.createElement('button');
-    tagButton.className =
-      'inline-view-toggle-button inline-view-toggle-button-' + format +
-      (viewType !== 'json' ? '' :
-        ' inline-view-toggle-button-selected inline-view-toggle-button-json-selected');
-    tagButton.textContent = format;
-    option.button(tagButton, viewType === format);
+  function createToggleWidget() {
+    const togglePanel = document.createElement('span');
+    togglePanel.className = 'inline-view-toggle-panel';
+    let carryValue;
 
-    togglePanel.appendChild(tagButton);
-    tagButton.onclick = () => {
-      toggleWidget.viewType = format;
-      option.onSelected?.();
+    const jsonButton = createTagToggle('json');
+    jsonButton.textContent = '{}';
+
+    jsonButton.onclick = () => {
+      params.state[params.path + TAG_VIEW_SUFFIX] = bindValue.view = 'json';
       params.invalidate();
     };
- }
+
+    /** @type {{ format: string, apply(value: any): FormatTagOption, button: HTMLElement }[]} */
+    const formatters = [];
+
+    for (const [format, ctor] of Object.entries(params.formats)) {
+      const button = createTagToggle(format);
+      togglePanel.appendChild(button);
+      const apply = ctor();
+      formatters.push({ format, apply, button });
+
+      button.onclick = () => {
+        params.state[params.path + TAG_VIEW_SUFFIX] = bindValue.view = format;
+        params.invalidate();
+      };
+    }
+
+    togglePanel.appendChild(jsonButton);
+
+    bindValue.view = 'json';
+
+    return bindValue;
+
+    function bindValue(value) {
+      carryValue = value;
+      const applied = formatters.map(fmt => fmt.apply(value));
+      let bestFormatIndex = -1;
+
+      const availableTags = [];
+
+      for (let iFormat = 0; iFormat < applied.length; iFormat++) {
+        const appliedFmt = applied[iFormat];
+        const fmt = formatters[iFormat];
+        if (appliedFmt.preference > 0) {
+          availableTags[iFormat] = true;
+          fmt.button.style.display = /** @type {*} */(null);
+          if (fmt.button.firstChild !== appliedFmt.button) {
+            fmt.button.textContent = '';
+            fmt.button.appendChild(appliedFmt.button);
+          }
+          if (bestFormatIndex < 0 || appliedFmt.preference > applied[bestFormatIndex].preference)
+            bestFormatIndex = iFormat;
+        } else {
+          availableTags[iFormat] = false;
+          fmt.button.style.display = 'none';
+        }
+      }
+
+      const stateView = params.state[params.path + TAG_VIEW_SUFFIX];
+      if (stateView) {
+        let retainStateFormatIndex = formatters.findIndex(fmt => fmt.format === stateView);
+        if (retainStateFormatIndex >= 0 && availableTags[retainStateFormatIndex] || stateView === 'json')
+          bestFormatIndex = retainStateFormatIndex;
+      }
+
+      if (bestFormatIndex < 0) {
+        params.state[params.path + TAG_VIEW_SUFFIX] = bindValue.view = 'json';
+        jsonButton.classList.add('inline-view-toggle-button-selected');
+      } else {
+        jsonButton.classList.remove('inline-view-toggle-button-selected');
+      }
+
+      for (let iFormat = 0; iFormat < applied.length; iFormat++) {
+        const appliedFmt = applied[iFormat];
+        const fmt = formatters[iFormat];
+        if (iFormat === bestFormatIndex) fmt.button.classList.add('inline-view-toggle-button-selected');
+        else fmt.button.classList.remove('inline-view-toggle-button-selected');
+      }
+
+      const subsequent = bestFormatIndex < 0 ?
+        params.json(value) :
+        applied[bestFormatIndex].render;
+
+      let combined =
+        !subsequent ? [{ widget: togglePanel }] :
+          Array.isArray(subsequent) ? [{ widget: togglePanel }, ...subsequent] :
+            [{ widget: togglePanel }, subsequent];
+
+      return combined;
+    }
+  }
+
+  /**
+   * @param {string} format
+   */
+  function createTagToggle(format) {
+    const tagButton = document.createElement('button');
+    tagButton.className =
+      'inline-view-toggle-button inline-view-toggle-button-' + format;
+    tagButton.textContent = format;
+    return tagButton;
+  }
 }
--- src/markdown/code-block/state/runtime/view/text/render-array.js
+++ src/markdown/code-block/state/runtime/view/text/render-array.js
@@ -1,8 +1,9 @@
 // @ts-check
 
-import { renderComposite } from './render-composite';
 import { collectColumns } from '../table/collect-columns';
 import { createTableView } from '../table/create-table-view-and-toggle';
+import { formatTagWidget } from './format-tags';
+import { renderComposite } from './render-composite';
 
 import './render-array.css';
 // @ts-ignore
@@ -14,80 +15,63 @@ import tableIconSvg from './table-icon.svg';
 export function renderArray(params) {
   const columns = params.value.length > 2 ? collectColumns(params.value) : undefined;
   if (columns) {
-    /** @type {'json' | 'table'} */
-    let arrayViewType = params.state[params.path + '.arrayViewType'] ?? 'table';
-
-    const toggleWidget = {
-      widget: () => {
-        const togglePanel = document.createElement('span');
-        togglePanel.className = 'inline-view-toggle-panel';
-        const tableButton = document.createElement('button');
-        tableButton.innerHTML = tableIconSvg;
-        const tableIcon = /** @type {SVGSVGElement} */(tableButton.querySelector('svg'));
-        tableIcon.setAttribute('class',  'svg-icon-tag svg-icon-tag-table');
-        tableIcon.style.width = '1.3em';
-        tableIcon.style.height = '1.3em';
-        const tableCaption = document.createElement('span');
-        tableCaption.textContent = ' ' + params.value.length.toLocaleString() + ' rows';
-        tableButton.appendChild(tableCaption);
-
-        tableButton.className =
-          'inline-view-toggle-button inline-view-toggle-button-table' +
-        (arrayViewType !== 'table' ? '' :
-            ' inline-view-toggle-button-selected inline-view-toggle-button-table-selected');
-        togglePanel.appendChild(tableButton);
-        tableButton.onclick = () => {
-          params.state[params.path + '.arrayViewType'] = 'table';
-          params.invalidate();
-        };
-
-        const jsonButton = document.createElement('button');
-        jsonButton.textContent = '{}';
-        jsonButton.className =
-          'inline-view-toggle-button inline-view-toggle-button-json' +
-        (arrayViewType !== 'json' ? '' :
-            ' inline-view-toggle-button-selected inline-view-toggle-button-json-selected');
-        togglePanel.appendChild(jsonButton);
-        jsonButton.onclick = () => {
-          params.state[params.path + '.arrayViewType'] = 'json';
-          params.invalidate();
-        };
-
-        return togglePanel;
-      }
-    };
-
-    /** @type {ReturnType<typeof createTableView>} */
-    let tableView = params.state[params.path + '.tableView'];
-    if (tableView) {
-      if (arrayViewType) {
-        tableView.rebind({
-          value: params.value,
-          indent: params.indent,
-          columns
+    const toggleWidget = formatTagWidget({
+      ...params,
+      json: value => {
+        return renderComposite({
+          ...params,
+          value
         });
+      },
+      formats: {
+        table: () => {
+          /** @type {ReturnType<typeof createTableView>} */
+          let tableView;
+          const btn = document.createElement('span');
+          btn.innerHTML = tableIconSvg;
+          const tableIcon = /** @type {SVGSVGElement} */(btn.querySelector('svg'));
+          tableIcon.setAttribute('class', 'svg-icon-tag svg-icon-tag-table');
+          tableIcon.style.width = '1.3em';
+          tableIcon.style.height = '1.3em';
+          const tableCaption = document.createElement('span');
+          tableCaption.textContent = ' ' + params.value.length.toLocaleString() + ' rows';
+          btn.appendChild(tableCaption);
+
+          return apply;
+
+          function apply(value) {
+            const columns = collectColumns(value);
+            if (!columns) return {
+              preference: 0,
+              button: btn,
+              render: []
+            };
+
+            if (!tableView) {
+              tableView = createTableView({
+                ...params,
+                columns,
+                value
+              });
+            } else {
+              tableView.rebind({
+                ...params,
+                columns,
+                value
+              });
+            }
+
+            return {
+              preference: 1,
+              button: btn,
+              render: { widget: () => tableView.panel }
+            };
+          }
+        }
       }
-    } else {
-      tableView = createTableView({
-        value: params.value,
-        indent: params.indent,
-        columns,
-        invalidate: params.invalidate
-      });
-      params.state[params.path + '.tableViewAndToggle'] = tableView;
-    }
+    });
 
-    /** @type {import('..').RenderedContent[]} */
-    let output = [toggleWidget];
-    if (arrayViewType === 'table') output.push({ widget: () => tableView.panel });
-
-    if (arrayViewType === 'table') {
-      params.wrap.availableHeight = 4;
-    } else {
-      output = output.concat(renderComposite(params));
-    }
-
-    return output;
+    return toggleWidget(params.value);
   }
 
   return renderComposite(params);
