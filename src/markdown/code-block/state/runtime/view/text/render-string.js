6709ec7b5094a60325bad4cf108e96632c8d1b14
Oleg Mihailik <mihailik@gmail.com>
2025-04-14 21:25:21 +0100

    Neater string renders: HTML and Markdown detection.

--- src/markdown/code-block/state/runtime/view/text/render-string.js
+++ src/markdown/code-block/state/runtime/view/text/render-string.js
@@ -3,9 +3,17 @@
 import { renderValue } from './render-value';
 import { parse } from '../../../../../../../node_modules/csv-parse/dist/esm/sync';
 
+import { markdownCodeToHTML } from '../../../../state-markdown/plugin-runtime';
+import { formatTagWidget } from './format-tags';
+
 import './render-string.css';
 
 const MIN_SIZE_FOR_INTERPRETING = 10;
+
+const regex_CR_or_LF = /[\r\n]/g;
+
+const regex_approximate_HTML = /<([a-zA-Z1-6]+)([^>]*)>.*?<\/\1>/s;
+const regex_approximate_Markdown = /^(#+ .*|>\s.*|\*\*\*|---\s?|\*\*\s.*\*\*\s?|\*\s.*\*\s?|__{2}.*_{2}\s?|_{1}.*_{1}\s?|\[.*?\]\(.*?\)|`.*?`|^\s{0,3}[-*+] .*|\d+\.\s.*)$|(?:\n(#+ .*|>\s.*|\*\*\*|---\s?|\*\*\s.*\*\*\s?|\*\s.*\*\s?|__{2}.*_{2}\s?|_{1}.*_{1}\s?|\[.*?\]\(.*?\)|`.*?`|^\s{0,3}[-*+] .*|\d+\.\s.*))$/m;
 
 /**
  * @param {import('.').ValueRenderParams<string>} params
@@ -16,6 +24,27 @@ export function renderString(params) {
   const interpreted = interpretString(value);
   if (!interpreted) {
     params.wrap.availableHeight = Math.max(0, params.wrap.availableHeight - 1);
+
+    if (value && value.length > 5 && regex_CR_or_LF.test(value)) {
+      const likelyMarkdown = regex_approximate_Markdown.test(value);
+      const likelyHTML = regex_approximate_HTML.test(value);
+
+      let renderText = value;
+      let renderHTML = likelyMarkdown || likelyHTML;
+
+      const toggleWidget = formatTagWidget({
+        ...params,
+        json: (params) => {
+          return [{ class: 'string-empty hi-string', textContent: !value ? '""' : JSON.stringify(value) }];
+        },
+        formats: {
+          text: createTextFormatter,
+          ...(renderHTML ? { html: createHTMLFormatter } : {})
+        }
+      });
+
+      return toggleWidget(params);
+    }
 
     // TODO: collapse too large string
     // TODO: provide option to render multiline strings unwrapped, without \n escapes
@@ -54,6 +83,54 @@ export function renderString(params) {
   }
 
   return renderedParsed;
+
+  function createTextFormatter() {
+    const btn = document.createElement('span');
+    btn.textContent = 'txt';
+    return apply;
+
+    function apply({ value }) {
+      return {
+        button: btn,
+        preference: 0.5,
+        render: () => {
+          const wrapper = document.createElement('div');
+          wrapper.className = 'rendered-string-plain-text';
+          const pre = document.createElement('pre');
+          pre.className = 'plain-text';
+          pre.textContent = value;
+          wrapper.appendChild(pre);
+          return wrapper;
+        }
+      };
+    }
+  }
+
+  function createHTMLFormatter() {
+    const btn = document.createElement('span');
+    btn.textContent = '*';
+    return apply;
+
+    function apply({ value }) {
+      return {
+        button: btn,
+        preference: 0.8,
+        render: () => {
+          const wrapper = document.createElement('div');
+          wrapper.className = 'rendered-string-html';
+          const host = document.createElement('div');
+          const likelyMarkdown = regex_approximate_Markdown.test(value);
+          if (likelyMarkdown) btn.textContent = 'MD';
+          else btn.textContent = 'HTML';
+          host.className = 'rendered-string-rich rendered-string-' + (likelyMarkdown ? 'markdown' : 'html');
+
+          host.innerHTML = likelyMarkdown ? markdownCodeToHTML(value) : value;
+          wrapper.appendChild(host);
+          return wrapper;
+        }
+      };
+    }
+  }
 }
 
 const MIN_CSV_ROWS = 2;
@@ -189,7 +266,7 @@ export function interpretString(str) {
           skip_empty_lines: true,
           cast: true
         });
-      
+
       if (parsed.length < MIN_CSV_ROWS) return;
 
       return { parsed, markers: ['CSV', 'CSV'] };
